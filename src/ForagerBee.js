1e5dcd3cccb58d72f4fab52a0b1b9dd922957c53
Sherman <shc410@gmail.com>
2016-09-19 11:17:20 -0700

    Implement rest of classes using pseudoclassical pattern

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-var ForagerBee = function() {
+var ForagerBee = function(treasureChest) {
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = treasureChest || [];
+	this.counter = 0;
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+	this.treasureChest[this.counter] = treasure;
+	this.counter++;
+};
--- src/Grub.js
+++ src/Grub.js
@@ -1,10 +1,10 @@
-var Grub = function(age, color, food, eat) {
+var Grub = function(age, color, food) {
 	this.age = age || 0;
 	this.color = color || 'pink';
 	this.food = food || 'jelly';
 };
 
 Grub.prototype.eat = function() {
-	
+
 };
 
