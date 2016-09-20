cca63dc4ed008fc1774832e701cfa139f6ff6ba0
Sherman <shc410@gmail.com>
2016-09-20 08:46:28 -0700

    Refactor RetiredForagerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,10 +1,10 @@
 var Bee = function(job) {
-	Grub.call(this);
+	// Grub.call(this);
 	this.age = 5;
 	this.color ='yellow';
 	this.job = job || "keep on growing";
 };
 
-Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype = new Grub;
 Bee.prototype.constructor = Bee;
 
